e68a26a2f5a279241792bc1513772fc306b994c1
Demétrius Mânica <demeteixeira@hotmail.com>
2019-11-28 13:59:39 -0300

    Implementing card generation functions for Uno challenge

--- lab/uno.ex
+++ lab/uno.ex
@@ -1,42 +1,93 @@
-
-# defmodule Card do
-#   defstruct card_type: ,
-# end
-
 defmodule Uno do
   def card_type(number) do
     case number do
       1 ->
-        'one'
+        "one"
       2 ->
-        'two'
+        "two"
       3 ->
-        'three'
+        "three"
       4 ->
-        'four'
+        "four"
       5 ->
-        'five'
+        "five"
       6 ->
-        'six'
+        "six"
       7 ->
-        'seven'
+        "seven"
+      8 ->
+        "eight"
+      9 ->
+        "nine"
+      10 ->
+        "reverse"
+      11 ->
+        "block"
+      12 ->
+        "plus-two"
+      13 ->
+        "wild"
+      14 ->
+        "wild-plus-four"
     end
   end
 
   def card_color(number) do
     case number do
       1 ->
-        'red'
+        "red"
       2 ->
-        'yellow'
+        "yellow"
       3 ->
-        'green'
+        "green"
       4 ->
-        'blue'
+        "blue"
+      5 ->
+        ""
     end
   end
 
+  defmodule Card do
+    defstruct card_type: Uno.card_type(13), card_color: Uno.card_color(4)
+  end
+
   def generate_random_card do
-    # card_type[:rand.uniform(14)] <> " " <> card_color[:rand.uniform(4)]
+    card_type_number = :rand.uniform(14)
+    IO.inspect(card_type_number)
+
+
+    card_color_number =
+      if card_type_number < 13 do
+        :rand.uniform(4)
+      else
+        5
+      end
+
+    %Uno.Card{ card_type: Uno.card_type(card_type_number), card_color: Uno.card_color(card_color_number) }
+  end
+
+  def print_card(card) do
+    case String.length(card.card_color) do
+      0 ->
+        card.card_type
+      _ ->
+        card.card_color <> " " <> card.card_type
+    end
+  end
+
+  def generate_random_hand do
+    hand = []
+    hand = hand ++ [Uno.generate_random_card]
+    hand = hand ++ [Uno.generate_random_card]
+    hand = hand ++ [Uno.generate_random_card]
+    hand = hand ++ [Uno.generate_random_card]
+    hand = hand ++ [Uno.generate_random_card]
+    hand = hand ++ [Uno.generate_random_card]
+    hand = hand ++ [Uno.generate_random_card]
+    hand
+  end
+
+  def playable_from_hand?(card, hand) do
+
   end
 end
