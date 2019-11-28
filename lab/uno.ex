d01069757b0b0c86a91c7013281d2914d0ae0a06
Demétrius Mânica <demeteixeira@hotmail.com>
2019-11-28 18:14:46 -0300

    Implemented playable_from_hand function

--- lab/uno.ex
+++ lab/uno.ex
@@ -48,7 +48,7 @@ defmodule Uno do
   end
 
   defmodule Card do
-    defstruct card_type: Uno.card_type(13), card_color: Uno.card_color(4)
+    defstruct card_type: "", card_color: ""
   end
 
   def generate_random_card do
@@ -66,7 +66,7 @@ defmodule Uno do
     %Uno.Card{ card_type: Uno.card_type(card_type_number), card_color: Uno.card_color(card_color_number) }
   end
 
-  def print_card(card) do
+  def card_to_string(card) do
     case String.length(card.card_color) do
       0 ->
         card.card_type
@@ -75,19 +75,52 @@ defmodule Uno do
     end
   end
 
-  def generate_random_hand do
-    hand = []
-    hand = hand ++ [Uno.generate_random_card]
-    hand = hand ++ [Uno.generate_random_card]
-    hand = hand ++ [Uno.generate_random_card]
-    hand = hand ++ [Uno.generate_random_card]
-    hand = hand ++ [Uno.generate_random_card]
-    hand = hand ++ [Uno.generate_random_card]
-    hand = hand ++ [Uno.generate_random_card]
-    hand
+  def print_card(card) do
+    IO.inspect(card_to_string(card))
   end
 
-  def playable_from_hand?(card, hand) do
+  def print_hand(hand) do
+    IO.puts("Current hand:")
+    hand_string = print_hand(hand, length(hand))
+  end
 
+  def print_hand(hand, hand_size) when hand_size <= 1 do
+   IO.inspect(card_to_string(hd(hand)))
+  end
+
+  def print_hand(hand, hand_size) do
+    IO.inspect(card_to_string(hd(hand)))
+    print_hand(tl(hand), hand_size - 1)
+  end
+
+  def add_random_cards_to_hand(hand, cards_count) when cards_count <= 1 do
+    hand = [Uno.generate_random_card | hand]
+  end
+
+  def add_random_cards_to_hand(hand, cards_count) do
+    hand = [Uno.generate_random_card | hand]
+    Uno.add_random_cards_to_hand(hand, cards_count - 1)
+  end
+
+  def playable_from_hand?(current_hand, face_up_card) do
+    check_hand_cards(current_hand, face_up_card)
+  end
+
+  def check_hand_cards([%Uno.Card{} = current_card | leftovers], face_up_card) do
+    IO.puts("face-up card: " <> card_to_string(face_up_card) <> " | current: " <> card_to_string(current_card))
+    cond do
+      current_card.card_type in ["wild", "wild-plus-four"] ->
+        true
+      current_card.card_type == face_up_card.card_type
+        true
+      current_card.card_color == face_up_card.card_color
+        true
+      true ->
+        check_hand_cards(leftovers, face_up_card)
+    end
+  end
+
+  def check_hand_cards([], face_up_card) do
+    false
   end
 end
